af0eb6f425d5fefd475d215a2bf2fb5802045a94
Arunachalam <arunachalam@crayond.co>
2025-07-22 23:09:26 +0530

    server as vercel

--- tele-health-backend/server.js
+++ tele-health-backend/server.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
 app.use("/", routes);
 
 app.listen(PORT, () => {
